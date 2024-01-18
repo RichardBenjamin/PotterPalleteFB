6ca3a0d88368865c313d245d1aea3c08aa1c098c
RichardBenjamin <146447026+RichardBenjamin@users.noreply.github.com>
2024-01-18 14:23:49 +0100

    first commit

--- Backend/Backend/app.js
+++ Backend/Backend/app.js
@@ -10,13 +10,12 @@ const cart = require("./routes/cart")
 const uploads = require("./routes/uploads")
 const cors = require("cors");
 
-const corsOptions = {
-  origin: ["http://127.0.0.1:5501", "http://127.0.0.1:5500", "http://127.0.0.1:5551", "http://127.0.0.1:5502","https://potter-pallete-fb.vercel.app/"],
-  methods: ["GET", "POST", "PUT", "PATCH","DELETE"],
-};
+// const corsOptions = {
+//   origin: ["http://127.0.0.1:5501", "http://127.0.0.1:5500", "http://127.0.0.1:5551", "http://127.0.0.1:5502","https://potter-pallete-fb.vercel.app/"],
+//   methods: ["GET", "POST", "PUT", "PATCH","DELETE"],
+// };
 const multer  = require('multer');
-// const cloudinary = require("cloudinary").v2;
-// const cloudinaryConfig = require()
+
 
 
 
@@ -24,7 +23,7 @@ const multer  = require('multer');
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 app.use(morgan("tiny"));
-app.use(cors(corsOptions));
+app.use(cors());
 app.use("/register", registerRoutes)
 app.use("/login", loginRoutes)
 app.use("/explore", exploreRoutes)
